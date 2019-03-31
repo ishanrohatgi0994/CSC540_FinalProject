10f9b45f993e4e158d81de155c4a707c755e86a5
Ishan Rohatgi <irohatg@ncsu.edu>
2019-03-31 18:03:34 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String args[]){
+
+        System.out.println("Establish DB connection here");
+
+        System.out.println("Write switch case here");
+    }
+}
