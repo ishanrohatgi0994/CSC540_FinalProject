527e1847a1145f1969c291a97bb2b8af708455a0
Harsh <harshshah949494@gmail.com>
2019-04-04 17:27:44 -0400

    Removing unwanted statements and imports

--- src/MedicalRecord.java
+++ src/MedicalRecord.java
@@ -1,4 +1,3 @@
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
--- src/establishConnection.java
+++ src/establishConnection.java
@@ -6,14 +6,12 @@ class establishConnection{
 	    	Class.forName("com.mysql.cj.jdbc.Driver");  
 	    	Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital","root","admin");  
 	    	Statement stmt=con.createStatement();  
-	    	ResultSet rs=stmt.executeQuery("select * from doctor");  
-	    	//while(rs.next())  
-	    		//System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+	    	stmt.executeQuery("select * from doctor");  
 	    	return con;  
     	}
     	catch(Exception e)
     	{ 
-    		System.out.println(e);
+    		System.out.println(e.getMessage());
     	}
 		return null;  
     }  
