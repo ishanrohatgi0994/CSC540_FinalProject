b468c1bfe95bca2377b5929817ed9b9e2416e358
Harsh <harshshah949494@gmail.com>
2019-04-12 18:56:32 -0400

    Added documentation for connection establishment and code for coninuous menu execution

--- src/establishConnection.java
+++ src/establishConnection.java
@@ -2,6 +2,8 @@ import java.sql.*;
     
 class establishConnection{  
     public Connection getConnection(){  
+    	// Establishes connection to our locally installed DB instance.
+    	// Returns the connection object
     	try{  
 	    	Class.forName("com.mysql.cj.jdbc.Driver");
 	    	//Connection con=DriverManager.getConnection("jdbc:mysql://10.154.59.5:3306/hospital?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","admin");
